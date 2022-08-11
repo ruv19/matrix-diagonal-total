7acab8e545b218b5166b605805da34757c059536
Rüveyda Dal <ruveydadal.3@gmail.com>
2022-08-11 16:45:00 +0300

    Update matris kosegen toplami.cpp

--- matris kosegen toplami.cpp
+++ matris kosegen toplami.cpp
@@ -1,36 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int main (void)
-{
-	int n,i,j,top=0;
+
+int main (void) {
+	int n, i, j, top=0;
 	printf("matrisin boyutunu giriniz:\n");
 	scanf("%d",&n);
 	int a[n][n];
 	
 	
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<n;j++)
-		{
+	for(i=0; i<n; i++) {
+		for(j=0; j<n; j++) {
+            
 			printf("a[%d][%d]=",i+1,j+1);
 			scanf("%d",&a[i][j]);
 		}
 	}
 	
-
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<n;j++)
-		{
-			if(i==j)
-			{
-				top=top+a[i][j];
+	for(i=0; i<n; i++) {
+		for(j=0; j<n; j++) {
+			if(i == j) {
+				top = top+a[i][j];
 			}
 		}
 	}
 	
-	
 	printf("\ntoplam=%d\n",top);
-
 }
